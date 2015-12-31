7b33fd1022d188047161450bcea638391c74b2c0
erikvw <ew2789@gmail.com>
2015-12-31 16:57:00 +0200

    fix edc_lab import error

--- edc_testing/models/test_requisition.py
+++ edc_testing/models/test_requisition.py
@@ -1,15 +1,16 @@
 from django.db import models
 
 from edc_base.audit_trail import AuditTrail
-from edc_lab.lab_requisition.models import BaseRequisition
+from edc_lab.lab_requisition.models import RequisitionModelMixin
 from edc.entry_meta_data.managers import RequisitionMetaDataManager
 
 from .test_visit import TestVisit
 from .test_panel import TestPanel
 from .test_aliquot_type import TestAliquotType
+from edc_visit_tracking.models.crf_model_mixin import CrfModelMixin
 
 
-class TestRequisition(BaseRequisition):
+class TestRequisition(CrfModelMixin, RequisitionModelMixin):
 
     test_visit = models.ForeignKey(TestVisit)
 
@@ -20,9 +21,6 @@ class TestRequisition(BaseRequisition):
     history = AuditTrail()
 
     entry_meta_data_manager = RequisitionMetaDataManager(TestVisit)
-
-    def get_visit(self):
-        return self.test_visit
 
     class Meta:
         app_label = 'edc_testing'
