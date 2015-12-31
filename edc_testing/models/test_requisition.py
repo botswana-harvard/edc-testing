4d13f9db2bf84e3558e5ef48451b629cc7012b13
erikvw <ew2789@gmail.com>
2015-12-31 17:03:29 +0200

    fix requisition

--- edc_testing/models/test_requisition.py
+++ edc_testing/models/test_requisition.py
@@ -8,9 +8,12 @@ from .test_visit import TestVisit
 from .test_panel import TestPanel
 from .test_aliquot_type import TestAliquotType
 from edc_visit_tracking.models.crf_model_mixin import CrfModelMixin
+from edc_testing.models.test_aliquot import TestAliquot
 
 
 class TestRequisition(CrfModelMixin, RequisitionModelMixin):
+
+    aliquot_model = TestAliquot
 
     test_visit = models.ForeignKey(TestVisit)
 
