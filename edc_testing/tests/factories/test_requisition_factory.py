5bf7706a7f882f0046527abed9d7ef2e54b9b085
erikvw <ew2789@gmail.com>
2016-01-13 14:56:45 +0200

    fix import

--- edc_testing/tests/factories/test_requisition_factory.py
+++ edc_testing/tests/factories/test_requisition_factory.py
@@ -1,6 +1,6 @@
 import factory
 
-from edc_lab.lab_requisition.tests.factories import BaseRequisitionFactory
+from lab_requisition.tests.factories import BaseRequisitionFactory
 
 from ...models import TestRequisition
 
