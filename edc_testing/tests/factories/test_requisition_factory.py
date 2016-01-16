110da700f6d3cea2e8b572821f5121ca863ab548
erikvw <ew2789@gmail.com>
2016-01-16 14:55:18 +0200

    update factory

--- edc_testing/tests/factories/test_requisition_factory.py
+++ edc_testing/tests/factories/test_requisition_factory.py
@@ -1,12 +1,15 @@
 import factory
 
-
-from ...models import TestRequisition
+from django.utils import timezone
 
 from edc_visit_tracking.tests.factories import TestVisitFactory
 
+from ...models import TestRequisition
+
 
 class TestRequisitionFactory(factory.DjangoModelFactory):
+
+    requisition_datetime = timezone.now()
 
     class Meta:
         model = TestRequisition
