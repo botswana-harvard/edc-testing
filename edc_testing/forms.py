d0444af275efc731e923b3239111e7a470802e42
SirOne Pharatlhatlhe <sirpharatlhatlhe@yahoo.co.uk>
2016-04-28 13:54:08 +0200

    python3/django1.9 inital work

--- edc_testing/forms.py
+++ edc_testing/forms.py
@@ -7,14 +7,17 @@ class TestSubjectUuidModelForm (forms.ModelForm):
 
     class Meta:
         model = TestSubjectUuidModel
+        fields = "__all__"
 
 
 class TestModelForm (forms.ModelForm):
 
     class Meta:
         model = TestModel
+        fields = "__all__"
 
 
 class TestScheduledModelForm(forms.ModelForm):
     class Meta:
         model = TestScheduledModel
+        fields = "__all__"
